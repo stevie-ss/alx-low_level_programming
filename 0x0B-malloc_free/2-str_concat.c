33b88acc262de2ea38f512a91848fac2941368a3
stevie-ss <sedziafasteve@gmail.com>
2023-03-14 17:25:52 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,14 +13,13 @@ char *str_concat(char *s1, char *s2)
 
 	int i, ci;
 
-	if (s1 == NULL)
 
+	if (s1 == NULL)
 		s1 = "";
 
+
 	if (s2 == NULL)
-
 		s2 = "";
-
 
 		i = ci = 0;
 
@@ -34,9 +33,10 @@ char *str_concat(char *s1, char *s2)
 
 	conct = malloc(sizeof(char) * (i + ci + 1));
 
-	if (conct == NULL
+	if (conct == NULL)
 		return (NULL);
 	i = ci = 0;
+	
 	while (s1[i] != '\0')
 	{
 		conct[i] = s1[i];
